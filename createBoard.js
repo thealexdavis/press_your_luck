0327ee11da9ab846004e430ba22e4aa11d2f6779
Alex Davis <alex.davis@saltwaterco.com>
2019-12-24 16:46:25 -0500

    Update

--- createBoard.js
+++ createBoard.js
@@ -1154,7 +1154,7 @@ function cycleTimer(theBoard) {
 function spinTimer() {
 	spinSquares = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
 	usedSquares = [];
-	spinVar = setInterval(spinBoard, 300);
+	spinVar = setInterval(spinBoard, 250);
 }
 
 function clearAllSquares(){
